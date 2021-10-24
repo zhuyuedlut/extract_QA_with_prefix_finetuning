e114cea9fbda98be34eed7978d4c672389a16607
zhuyuedlut <516872091@qq.com>
2021-10-24 23:28:04 +0800

    feat: add prefix tuning

--- src/models/prefix_tuning.py
+++ src/models/prefix_tuning.py
@@ -1,56 +1,19 @@
-import logging
-import os
-from argparse import ArgumentParser
+import torch
+import torch.nn as nn
 
-from pathlib import Path
-from typing import Optional
-
-import pytorch_lightning as pl
-from datasets import load_dataset
-from transformers import AutoConfig, AutoTokenizer
-
-from model_prefix_bart import BartModel
-
-logger = logging.getLogger(__name__)
+from transformers import PretrainedBartModel
 
 
-class PrefixBart(pl.LightningModule):
-    def __init__(self,
-                 model_name_or_path,
-                 config=None,
-                 **config_kwargs,
-                 ):
-        super().__init__()
+class PrefixTuning(PretrainedBartModel):
+    def __init__(self, config):
+        super(PrefixTuning, self).__init__(config)
+        self.preseqlen = config.preseqlen
+        self.mid_dim = config.mid_dim
+        self.n_embd = config.d_model
 
-        self.save_hyperparameters()
-
-        self.output_dir = Path(self.hparams.output_dir)
-        cache_dir = self.hparams.cache_dir if self.hparams.cache_dir else None
-
-        if config is None:
-            self.config = AutoConfig.from_pretrained(
-                self.hparams.config_name if self.hparams.config_name is not None else self.hparams.model_name_or_path,
-                cache_dir=cache_dir,
-                **config_kwargs,
-            )
-        else:
-            self.config = config
-
-    def forward(self, **inputs):
-        return self.model(inputs)
-
-    def training_step(self, batch, batch_idx):
-        outputs = self(**batch)
-        loss = outputs[0]
-        return loss
-
-    # def validation_step(self, *args, **kwargs) -> Optional[STEP_OUTPUT]:
-    #
-    # def test_step(self, *args, **kwargs) -> Optional[STEP_OUTPUT]:
-
-    @staticmethod
-    def add_model_specific_args(parent_parse):
-        parser = ArgumentParser(parents=[parent_parse])
-        parser.add_argument("--pre_len", default=200, type=int,
-                            help="The prefix embedings length")
-        return parser
+        self.input_tokens = torch.arange(self.preseqlen).long()
+        self.wte = nn.Embedding(self.preseqlen, self.n_embd)
+        self.prefix_attn = nn.Sequential(
+            nn.Linear(self.n_embd, self.mid_dim),
+            nn.Tanh(),
+        )
